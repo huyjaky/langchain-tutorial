b9eaf9c05bbfeffda31193397ba8de382509dc8c
huyjaky <huyphanuyg@gmail.com>
2026-05-10 16:50:04 +0700

    RAG

--- agent-with-RAG-vector-store.py
+++ agent-with-RAG-vector-store.py
@@ -1,7 +1,37 @@
 from dotenv import load_dotenv
+import os
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import PGVector
 
 load_dotenv()
 
+
+# read env variables
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL", "")
+OPENAI_MODEL_IDENTIFIER = os.getenv("OPENAI_MODEL_IDENTIFIER", "")
+CONNECTION_STRING = os.getenv("PGVECTOR_CONNECTION_STRING")  # Or whatever you named it
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-bge-m3",
+    base_url=OPENAI_BASE_URL,
+    api_key=OPENAI_API_KEY,  # pyright: ignore
+    check_embedding_ctx_length=False,
+)
+
+texts = [
+    "The cat is on the table.",
+    "The dog is in the garden.",
+    "The bird is flying in the sky.",
+    "The fish is swimming in the pond.",
+]
+
+vector_store = PGVector.from_texts(
+    texts,
+    embeddings,
+    collection_name="test_collection",
+    connection_string=CONNECTION_STRING,
+)
+
+print(vector_store.similarity_search("Where is the cat?", k=2))
